4de598428a34c3e4bc4f4556de5c811b52820019
Fikri Aqsha <m.fikriaqsha@gmail.com>
2025-12-25 11:33:56 +0700

    request to github API to get the user's activities

--- bin/index.js
+++ bin/index.js
@@ -3,3 +3,22 @@
 const args = process.argv.slice(2);
 
 const username = args[0];
+
+async function getGithubActivity() {
+
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}/events`);
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+
+    } catch (err) {
+        console.error(`${err.name}: ${err.message}`);
+    }
+}
+
+const result = await getGithubActivity();
